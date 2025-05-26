59f59ad3723a6d7f584f837572d3d26357925162
Milagros Ranaldi <mranaldi44@gmail.com>
2025-05-26 16:53:26 -0300

    filtrado y paginado de facturas ok

--- src/modules/facturas/controllers/facturas.controller.ts
+++ src/modules/facturas/controllers/facturas.controller.ts
@@ -29,7 +29,7 @@ export class FacturasController {
 findAll(
   @Request() req,
   @Query('page') page: number = 1,
-  @Query('limit') limit: number = 10,
+  @Query('limit') limit: number = 5,
   @Query('order') order: 'ASC' | 'DESC' = 'DESC',
   @Query('fecha') fecha?: string, // formato YYYY-MM-DD
 ) {
